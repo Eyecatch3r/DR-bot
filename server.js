ded360baef138bfcb3418dadd02748df56654229
Glitch (starter-discord) <none>
2020-04-17 17:17:58 +0000

    🍳😋 Checkpoint ./server.js:15114350/1669

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var Client = require("uptime-robot");
 const app = express();
 
 const discordBot = require("./bot");
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const clientdc = new Discord.Client();
-clientdc.login(process.env.)
+clientdc.login('NzAwNjYyNDY0ODU2OTgxNTY0.XpnikQ.pHMQ0NYjTeYp-YSvf3D0tav0qCI');
 // this is the code for the guides
-app.use(require('./guides'));
+app.use(require("./guides"));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -22,3 +22,5 @@ app.use(express.static("public"));
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+clientdc 

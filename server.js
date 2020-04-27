004159cfe5136dba5a1693fa0bfe7a4e2b6a01ea
Glitch (discord-romanum) <none>
2020-04-27 21:55:23 +0000

    🎄👮 Checkpoint ./server.js:15114350/8

--- server.js
+++ server.js
@@ -250,7 +250,7 @@ clientdc.on("message", message => {
       message.channel.send("thats not a Role Broski");
     }
   }
-  if (message === "?pray") {message.channel.send("Frank get to work");}
+  if (message.content === "?pray") {message.channel.send("Frank get to work");}
 });
 
 clientdc.on("message", async message => {

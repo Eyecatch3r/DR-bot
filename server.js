b78a2ea24534a183d2a6c5d4fe411c73de1cd2f9
Glitch (discord-romanum) <none>
2020-04-30 11:27:49 +0000

    🌉👯 Checkpoint ./server.js:15114350/26

--- server.js
+++ server.js
@@ -167,6 +167,8 @@ clientdc.on("message", message => {
       switch(args[1])
         {
           case 'consul':
+            
+            
             clientdc.channels.cache.get('548918811391295489').send('consul Elections react here with 🔴').then(m => {
     const filter = (reaction, user) => user.id === message.author.id && reaction.emoji.name === '🔴';
               m.react("🔴");

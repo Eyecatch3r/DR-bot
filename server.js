e1ad0c29eadacd9fbf0d3cb352eac0ed2a0359b3
Glitch (discord-romanum) <none>
2020-05-09 20:23:41 +0000

    ☔️🍜 Checkpoint ./server.js:15114350/35

--- server.js
+++ server.js
@@ -544,8 +544,8 @@ for (i = 0; i < rows.length; i++) {
               if (reaction.emoji.name == lett[i]) {
                 rows.forEach(row => {
                   if (can[i] == row.DiscordID) {
-                    db.get("SELECT COUNT() AS count FROM Voters JOIN votercandidate ON vID = voter JOIN candidateElections ON votercandidate.candidate = candidateElections.candidate JOIN Elections ON Election = eID WHERE voter IN (SELECT vID FROM Voters WHERE DiscordID = "+user.id+") AND Election IN (SELECT eID WHERE Month = "+args[2]+")",[],(err,rowt) => {
-                      if(rowt.count <= maxVote || rowt.count == undefined){
+                    db.all("SELECT COUNT() AS count FROM Voters JOIN votercandidate ON vID = voter JOIN candidateElections ON votercandidate.candidate = candidateElections.candidate JOIN Elections ON Election = eID WHERE voter IN (SELECT vID FROM Voters WHERE DiscordID = "+user.id+") AND Election IN (SELECT eID WHERE Month = "+args[2]+")",[],(err, rowt) => {
+                      if(rowt[0].count <= maxVote || rowt[0].count == undefined){
                    
                     db.run(
                       "UPDATE CandidateElections SET votes = votes+1 WHERE candidate IN (SELECT cID FROM candidates WHERE DiscordID = "+row.DiscordID+")  AND Election IN (SELECT eID FROM Elections WHERE Month = '"+row.Month+"')"

b32ddad888862200e5be84220fcfbd6c0de21ac3
Glitch (discord-romanum) <none>
2020-06-14 16:20:39 +0000

    👵🛋 Checkpoint ./server.js:15114350/18 ./package.json:15114350/26

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const wikiFacts = require('wikifakt');
 const tr = require('translate');
 tr.key = 'trnsl.1.1.20200515T222139Z.a68c11f6ccb16bd4.b2bb25b5f6b3c103701bbe0015c7998ab237fa98';
 tr.engine = 'yandex';
-app.use("/assets", assets);
+//app.use("/assets", assets);
 
 const discordBot = require("./bot");
 const Discord = require("discord.js");
@@ -37,7 +37,7 @@ const clientdc = new Discord.Client();
 
 
 clientdc.login(process.env.DISCORD_TOKEN);
-app.use(require("./guides"));
+//app.use(require("./guides"));
 
 var MotionListChannel;
 
@@ -1665,12 +1665,12 @@ clientdc.on("messageUpdate", message => {
 });
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+//app.use(express.static("public"));
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+//const listener = app.listen(process.env.PORT, function() {
+  //console.log("Your app is listening on port " + listener.address().port);
 
-  setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-  }, 280000);
-});
+  //setInterval(() => {
+   // http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  //}, 280000);
+//});
